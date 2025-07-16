fb3e858a4f1399c72862d827c0ab845ba2b9fbd9
Hilmy Azkal A <155927836+azkalhilmy@users.noreply.github.com>
2025-07-16 13:58:27 +0700

    Update Shp_geojson.py

--- Script Python/Shp_geojson.py
+++ Script Python/Shp_geojson.py
@@ -7,5 +7,3 @@ gdf = gpd.read_file(shp_path)
 # Simpan ke GeoJSON
 geojson_path = "/mnt/d/ProjectOSM/Raw/Kecamatan.geojson"
 gdf.to_file(geojson_path, driver="GeoJSON")
-
-print("Konversi selesai! File tersimpan di:", geojson_path)
