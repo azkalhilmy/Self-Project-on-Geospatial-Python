405144580a2a90242746404b7e7d40f489e848ff
Hilmy Azkal A <155927836+azkalhilmy@users.noreply.github.com>
2025-07-16 23:51:19 +0700

    Update Betweenness(K=100).py

--- Script Python/Betweenness(K=100).py
+++ Script Python/Betweenness(K=100).py
@@ -2,21 +2,21 @@ import geopandas as gpd
 import networkx as nx
 from shapely.geometry import Point
 
-# === Load edges dari geojson ===
+#Load edges format geojson
 edges = gpd.read_file("/mnt/d/ProjectOSM/Raw/jalan_edges2.geojson")
 
-# === Bangun graf dari edges ===
+#Bangun graf dari edges
 G = nx.Graph()
 for _, row in edges.iterrows():
     u = (row['from_x'], row['from_y'])
     v = (row['to_x'], row['to_y'])
     G.add_edge(u, v, weight=row['weight'], highway=row.get('highway'), name=row.get('name'))
 
-# === Analisis Betweenness Centrality ===
+#Analisis Betweenness Centrality
 print("Proses menghitung approx betweenness centrality dengan k=100")
 bet_centrality = nx.betweenness_centrality(G, k=100, weight='weight', normalized=True, seed=42)
 
-# Buat GeoDataFrame simpul dengan nilai betweenness
+#Buat GeoDataFrame simpul dengan nilai betweenness
 node_geoms = []
 node_bet = []
 for node, val in bet_centrality.items():
